6191f5ba98e241254b3ca0e5af2fb8fca177aa86
jonnym <jonnym>
2025-07-10 09:44:27 +0100

    added logging and username extraction
    Signed-off-by: jonnym <jonnym>

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ setup(
     name="cognito-jupyterhub-auth",
     version="0.1.0",
     description="JWT-based JupyterHub authenticator for AWS Cognito",
-    author="Jonny Melia",
+    author="Your Name",
     author_email="your.email@example.com",
     py_modules=["jwt_authenticator"],
     install_requires=[
